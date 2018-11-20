03c34c470d2d97774fac6bb059e721d1e4964e61
aseev <asdnvx14@yahoo.com>
2018-11-20 21:18:34 +0600

    video2

--- js/video.js
+++ js/video.js
@@ -18,28 +18,11 @@ function showVideoListChannel(channelid, writediv, maxnumbervideos, apikey) {
         var vid = getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" + channelid + "&maxResults=" + (maxnumbervideos + 1) + "&key=" + apikey);
         var videoinfo = JSON.parse(vid);
         var videos = videoinfo.items;
-        var videocount = videoinfo.pageInfo.totalResults;
         var content = "<div class='video-gallery gallery-flex-container'>";
         for (var i = 0; i < videos.length - 1; i++) {
             var videoid = videos[i].id.videoId;
-            var videotitle = videos[i].snippet.title;
-            var videodescription = videos[i].snippet.description;
-            var videodate = videos[i].snippet.publishedAt; // date time published
-            var newdate = new Date(Date.parse((videodate + " (ISO 8601)").replace(/ *\(.*\)/, "")));
-            var min = newdate.getMinutes();
-            if (min < 10) {
-                min = "0" + min;
-            }
-            if (newdate.getHours() > 12) {
-                newdate = newdate.getMonth() + 1 + "/" + newdate.getDate() + "/" + newdate.getFullYear() + " " + (newdate.getHours() - 12) + ":" + min + " PM";
-            } else if (newdate.getHours() == 12) {
-                newdate = newdate.getMonth() + 1 + "/" + newdate.getDate() + "/" + newdate.getFullYear() + " " + newdate.getHours() + ":" + min + " PM";
-            } else {
-                newdate = newdate.getMonth() + 1 + "/" + newdate.getDate() + "/" + newdate.getFullYear() + " " + newdate.getHours() + ":" + min + " AM";
-            }
-            var videothumbnail = videos[i].snippet.thumbnails.default.url; // default, medium or high
             content += "<div class='gallery-flex-item'>"
-                + "<iframe id='player" + i + "' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&origin=https://qwerty11aa.github.io/FEF/' frameborder='0' allow='picture-in-picture;'>"
+                + "<iframe id='player" + i + "'class='youtube-player' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&html5=1' frameborder='0' allow='picture-in-picture;'>"
                 + "</iframe>"
                 + "</div>";
         }
@@ -50,17 +33,26 @@ function showVideoListChannel(channelid, writediv, maxnumbervideos, apikey) {
     }
 }
 
-function onPlayerStateChange() {
-    console.log( "ready!" );
-  };
-
-$( document ).ready(function() {
-    var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player1', {
-    events: {
-      'onStateChange': onPlayerStateChange
+function onPlayerStateChange(currentPlyaer) {
+    for (var i = 0; i < players.length; i++) {
+        var test = players[i].getPlayerState()
+        if (players[i].getPlayerState() = 1)
+        {
+            players[i].stopVideo();
+        }
     }
+};
+
+var players = new Array();
+function onYouTubeIframeAPIReady() {
+    var i = 0;
+    $('.youtube-player').each(function(){
+        test = this;
+        players[i] = new YT.Player(this.id, {
+            events: {
+                'onStateChange': onPlayerStateChange(this)
+            }   
+    });
+    i++;    
   });
 }
-});
