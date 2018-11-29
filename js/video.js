d87dbd723cd655df24bee5c2857cc336432ac24a
aseev <asdnvx14@yahoo.com>
2018-11-29 21:54:10 +0600

    fixed video

--- js/video.js
+++ js/video.js
@@ -1,5 +1,5 @@
 // Youtube related functinos
-
+showVideoListChannel("UCC552Sd-3nyi_tk2BudLUzA", "videos", 10, "AIzaSyDqU1azV11Vf_ebUzVr3OnebiRrx5ChiQo");
     
 function getJSONData(yourUrl) {
     var Httpreq = new XMLHttpRequest();
@@ -15,14 +15,17 @@ function getJSONData(yourUrl) {
 function showVideoListChannel(channelid, writediv, maxnumbervideos, apikey) {
     try {
         document.getElementById(writediv).innerHTML = "";
-        var vid = getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" + channelid + "&maxResults=" + (maxnumbervideos + 1) + "&key=" + apikey);
+        var vid = getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" 
+        + channelid 
+        + "&maxResults=" + (maxnumbervideos + 1) 
+        + "&key=" + apikey);
         var videoinfo = JSON.parse(vid);
         var videos = videoinfo.items;
         var content = "<div class='video-gallery gallery-flex-container'>";
         for (var i = 0; i < videos.length - 1; i++) {
             var videoid = videos[i].id.videoId;
             content += "<div class='gallery-flex-item'>"
-                + "<iframe data-array-index='"+i+"' id='Player" + i + "'class='youtube-video' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&html5=1' frameborder='0' allow='picture-in-picture;'>"
+                + "<iframe data-array-index='"+i+"' id='Player" + i + "'class='youtube-video' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&html5=1&showinfo=0&controls=0' frameborder='0' allow='picture-in-picture;'>"
                 + "</iframe>"
                 + "</div>";
         }
@@ -51,16 +54,36 @@ window.onYouTubeIframeAPIReady = function(){
     });
     i++;
   });
+  window.onscroll = function () { scrollFunction() };
 };
 
-    function onPlayerStateChange(player_index){
-        return function(event) {
-            players.forEach(function(item, i, players) {
-                var palyerState = players[i].getPlayerState();
-                var currentPlayerState = players[player_index].getPlayerState();
-                if(palyerState == 1 && currentPlayerState == 1 && i!=player_index) {
-                    players[i].stopVideo()
-                }
-                });
+function onPlayerStateChange(player_index){
+    return function(event) {
+        players.forEach(function(item, i, players) {
+            var palyerState = players[i].getPlayerState();
+            var currentPlayerState = players[player_index].getPlayerState();
+            if(palyerState == 1 && currentPlayerState == 1 && i!=player_index) {
+                players[i].stopVideo();
             }
-    }
+            });
+        }
+}
+
+function scrollFunction() {
+    if ($(window).scrollTop() < 1500) {
+        players.forEach(function(item, i, players) {
+            var palyerState = players[i].getPlayerState();
+            if(players[i].getPlayerState() == 1) {
+                var playerId = '#Player'+i;
+                $(playerId).addClass('fixedposition');
+            }
+        });
+    } else {
+        players.forEach(function(item, i, players) {
+            if(players[i].getPlayerState() == 1) {
+                var playerId = '#Player'+i;
+                $(playerId).removeClass('fixedposition');
+            }
+        });
+        }
+}
