dd571258d2d4f94d1b436610381e9487168620d5
aseev <asdnvx14@yahoo.com>
2018-12-03 17:18:43 +0600

    Video Module corrections

--- js/video.js
+++ js/video.js
@@ -1,42 +1,49 @@
 // Youtube related functinos
-showVideoListChannel("UCC552Sd-3nyi_tk2BudLUzA", "videos", 10, "AIzaSyDqU1azV11Vf_ebUzVr3OnebiRrx5ChiQo");
-    
-function getJSONData(yourUrl) {
-    var Httpreq = new XMLHttpRequest();
-    try {
-        Httpreq.open("GET", yourUrl, false);
-        Httpreq.send(null);
-    } catch (ex) {
-        alert(ex.message);
-    }
-    return Httpreq.responseText;
-}
-
-function showVideoListChannel(channelid, writediv, maxnumbervideos, apikey) {
-    try {
-        document.getElementById(writediv).innerHTML = "";
-        var vid = getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" 
-        + channelid 
-        + "&maxResults=" + (maxnumbervideos + 1) 
-        + "&key=" + apikey);
-        var videoinfo = JSON.parse(vid);
-        var videos = videoinfo.items;
-        var content = "<div class='video-gallery gallery-flex-container'>";
-        for (var i = 0; i < videos.length - 1; i++) {
-            var videoid = videos[i].id.videoId;
-            content += "<div class='gallery-flex-item'>"
-                + "<iframe data-array-index='"+i+"' id='Player" + i + "'class='youtube-video' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&html5=1&showinfo=0&controls=0' frameborder='0' allow='picture-in-picture;'>"
-                + "</iframe>"
-                + "</div>";
+var youtubeVideoCreator = (function () {
+    function getJSONData(yourUrl) {
+        var Httpreq = new XMLHttpRequest();
+        try {
+            Httpreq.open("GET", yourUrl, false);
+            Httpreq.send(null);
+        } catch (ex) {
+            alert(ex.message);
         }
-        content += "</div>";
-        document.getElementById(writediv).innerHTML = content;
-    } catch (ex) {
-        alert(ex.message);
-    }
-}
+        return Httpreq.responseText;
+    };
+    
+    var showVideoListChannel = function(channelid, maxnumbervideos, apikey) {
+        try {
+            var vid = getJSONData("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=" 
+            + channelid 
+            + "&maxResults=" + (maxnumbervideos + 1) 
+            + "&key=" + apikey);
+            var videoinfo = JSON.parse(vid);
+            var videos = videoinfo.items;
+            var content = "<div class='video-gallery gallery-flex-container'>";
+            for (var i = 0; i < videos.length - 1; i++) {
+                var videoid = videos[i].id.videoId;
+                content += "<div class='gallery-flex-item'>"
+                    + "<iframe data-array-index='"+i+"' id='Player" + i + "'class='youtube-video' src='https://www.youtube.com/embed/" + videoid + "?enablejsapi=1&html5=1&showinfo=0&controls=0' frameborder='0' allow='picture-in-picture;'>"
+                    + "</iframe>"
+                    + "</div>";
+            }
+            content += "</div>";
+            return content;
+        } catch (ex) {
+            alert(ex.message);
+        }
+    };
 
-var tag = document.createElement('script');
+    return {
+        addVideosToElementById : showVideoListChannel
+    }
+})();
+
+document.getElementById("videos").innerHTML = youtubeVideoCreator.addVideosToElementById("UCC552Sd-3nyi_tk2BudLUzA", 6, "AIzaSyDqU1azV11Vf_ebUzVr3OnebiRrx5ChiQo");
+
+
+(function() {
+    var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -68,11 +75,11 @@ function onPlayerStateChange(player_index){
             });
         }
 }
+})();
 
 function scrollFunction() {
     if ($(window).scrollTop() < 1500) {
         players.forEach(function(item, i, players) {
-            var palyerState = players[i].getPlayerState();
             if(players[i].getPlayerState() == 1) {
                 var playerId = '#Player'+i;
                 $(playerId).addClass('fixedposition');
@@ -80,8 +87,8 @@ function scrollFunction() {
         });
     } else {
         players.forEach(function(item, i, players) {
-            if(players[i].getPlayerState() == 1) {
-                var playerId = '#Player'+i;
+            var playerId = '#Player'+i;
+            if($(playerId).hasClass('fixedposition')) {
                 $(playerId).removeClass('fixedposition');
             }
         });
